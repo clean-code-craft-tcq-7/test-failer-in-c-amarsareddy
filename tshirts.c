57dffff7abfb32411e0e3e6543fabf4d89521ed6
amarsareddy <66358468+amarsareddy@users.noreply.github.com>
2025-08-03 15:02:23 +0530

    Restructured Code and added testcases

--- tshirts.c
+++ tshirts.c
@@ -2,22 +2,30 @@
 #include <assert.h>
 
 char size(int cms) {
-    char sizeName = '\0';
-    if (cms < 38) {
-        sizeName = 'S';
-    } else if (cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if (cms > 42) {
-        sizeName = 'L';
+    char sizename = '\0';
+    if(cms > 42){
+        sizename = 'L';
     }
-    return sizeName;
+    else if(cms > 38){
+       sizename = 'M'; 
+    }
+    else{
+        sizename = 's';
+    }
+    return sizename;
 }
+
+
 
 int testTshirtSize() {
     printf("\nTshirt size test\n");
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
+    assert(size(37) == 'S');               // To test S size valid class
+    assert(size(38) == 'S');               // To test the max boundary of the Short size
+    assert(size(39) == 'M');               // To test M size Minimum boundary check
+    assert(size(40) == 'M');               // To test M size valid class
+    assert(size(42) == 'M');               // To test M size maximum boundary check
+    assert(size(43) == 'L');               // To test L size Minimum boundary check
+    assert(size(45) == 'L');               // To test L size valid class and Minimum boundary check
     printf("All is well (maybe!)\n");
     return 0;
 }
