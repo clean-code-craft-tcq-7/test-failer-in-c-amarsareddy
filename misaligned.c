63d18d14dcc0005b6119d8fdb9bd586b02002ba3
amarsareddy <66358468+amarsareddy@users.noreply.github.com>
2025-08-04 23:01:08 +0530

    Updated Code and testcase

--- misaligned.c
+++ misaligned.c
@@ -1,75 +1,89 @@
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
 
 struct 
 {
    int Colour_Index[25];
-   char MajorColour[25][7];
-   char MinorColour[25][7];
-}Store_Colour_Comb;
-
-
-#include <stdio.h>
-#include <assert.h>
+   char MajorColour[25][8]; 
+   char MinorColour[25][8];
+} Store_Colour_Comb;
 
 int printColorMap() {
     const char* majorColor[] = {"White ", "Red   ", "Black ", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue  ", "Orange", "Green ", "Brown ", "Slate"};
-    int i = 0, j = 0,k = 0, p = 0;
+    int i = 0, j = 0, k = 0, p = 0 , len = 0;
+
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
-            
             k = (i * 5 + j);
-            if((k)<9)
-            {
-                printf("%d  | %s | %s\n", (k+1), majorColor[i], minorColor[j]);
-            }
+            if (k < 9)
+                printf("%d  | %s | %s\n", k + 1, majorColor[i], minorColor[j]);
             else
-            {
-                printf("%d | %s | %s\n", ((i * 5 + j)+1), majorColor[i], minorColor[j]);
-            } 
-            Store_Colour_Comb.Colour_Index[k]= (k+1);
-           // printf("%d is colour index \n",Store_Colour_Comb.Colour_Index[k]);
-            
-            
-            // Copy majorColor[i] to structure manually
-            for ( p = 0; p < 7; p++) {
+                printf("%d | %s | %s\n", k + 1, majorColor[i], minorColor[j]);
+
+            Store_Colour_Comb.Colour_Index[k] = k + 1;
+
+            for (p = 0; p < 7; p++) {
                 Store_Colour_Comb.MajorColour[k][p] = majorColor[i][p];
             }
+            Store_Colour_Comb.MajorColour[k][7] = '\0';
 
-            // Copy minorColor[j] to structure manually
             for (p = 0; p < 7; p++) {
                 Store_Colour_Comb.MinorColour[k][p] = minorColor[j][p];
             }
-            
+            Store_Colour_Comb.MinorColour[k][7] = '\0';
         }
     }
+    
     return i * j;
 }
 
 int testPrintColorMap() {
+    int i,j,len,Equality_Of_Strings = 1,result;
+
+    const char* string_Expected_MajColour[25] = {
+        "White ", "White ", "White ", "White ", "White ",
+        "Red   ", "Red   ", "Red   ", "Red   ", "Red   ",
+        "Black ", "Black ", "Black ", "Black ", "Black ",
+        "Yellow", "Yellow", "Yellow", "Yellow", "Yellow",
+        "Violet", "Violet", "Violet", "Violet", "Violet"
+    };
+
+    const char* string_Expected_MinColour[25] = {
+        "Blue  ", "Orange", "Green ", "Brown ", "Slate",
+        "Blue  ", "Orange", "Green ", "Brown ", "Slate",
+        "Blue  ", "Orange", "Green ", "Brown ", "Slate",
+        "Blue  ", "Orange", "Green ", "Brown ", "Slate",
+        "Blue  ", "Orange", "Green ", "Brown ", "Slate"
+    };
+
+   result = printColorMap();
     
-    char String1[25][6] = {"1  |","2  |","3  |","4  |","5  |","6  |","7  |","8  |","9  |","10  |","11  |","12  |","13  |","14  |","15  |","16  |","17  |","18  |","19  |","20  |","21  |","22  |","23  |","24  |","25  |"};
+    for (i = 0; i < 25; i++) {
+        if (strcmp(string_Expected_MinColour[i], Store_Colour_Comb.MinorColour[i]) != 0) {
+            printf("Mismatch at index %d:\nExpected: %s\nActual  : %s\n", 
+                   i + 1, string_Expected_MinColour[i], Store_Colour_Comb.MinorColour[i]);
+            Equality_Of_Strings = 0;
+        }
+    }
     
-    char string_MajColour[25][8] = {" White  |"," White  |"," White  |"," White  |"," White  |"," Red    |"," Red    |"," Red    |"," Red    |"," Red    |"," Black  |"," Black  |"," Black  |"," Black  |"," Black  |"," Yellow |"," Yellow |"," Yellow |","Yellow |","Yellow |"," Violet |"," Violet |"," Violet |"," Violet |"," Violet |"};
+    for (i = 0; i < 25; i++) {
+    if (strcmp(string_Expected_MajColour[i], Store_Colour_Comb.MajorColour[i]) != 0) {
+        printf("Mismatch at index %d:\nExpected: %s\nActual  : %s\n", 
+               i + 1, string_Expected_MinColour[i], Store_Colour_Comb.MinorColour[i]);
+        Equality_Of_Strings = 0;
+    }
+    }
+
+    if(Equality_Of_Strings == 1)
+    {
+        printf("Testcase passed, Calculated Values are same as expected");
+    }
+    else
+    {
+        printf("Testcase failed, Calculated Values are not same as expected");
+    }
     
-    char string_MinColour[25][9] = {" Blue |"," Orange |"," Green  |"," Brown |"," Slate |"," Blue |"," Orange |"," Green  |"," Brown |"," Slate |"," Blue |"," Orange |"," Green  |"," Brown |"," Slate |"," Blue |"," Orange |"," Green  |"," Brown |"," Slate |"," Blue |"," Orange |"," Green  |"," Brown |"," Slate |"};
-    
-    printf("\nPrint color map test\n");
-    //String1[0] = printColorMap();
-    //assert(String1[0] == 25);
-    printf("%d is the result\n",String1);
-    printf("All is well (maybe!)\n");
     return 0;
 }
-
-
-void main()
-{
-    //testPrintColorMap();
-    printColorMap();
-    
-}
-
-
-
